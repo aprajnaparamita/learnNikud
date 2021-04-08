87c800bb9f5e5a63cc56ab762b292e3f76ca4441
Janet Braswell-Jeffus <speak@jjeff.us>
2021-04-08 12:40:37 +0700

    adding home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonPage, IonTitle, IonImg, IonList, IonItem, IonThumbnail, IonButton, IonCard, IonCardContent} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <img src="/logo.png" width="50%" id="logo" />
+        <IonCard>
+          <IonButton expand="block" class="home_button" color="danger">Introduction</IonButton>
+        </IonCard>
+        <IonCard>
+          <IonButton expand="block" class="home_button" color="warning">Practice</IonButton>
+        </IonCard>
+        <IonCard>
+          <IonButton expand="block" class="home_button" color="success">Test</IonButton>
+        </IonCard>
+        <IonCard>
+        </IonCard>
+        <IonCard button="true" href="https://apps.apple.com/us/app/alef-bet-wheel/id1088249022">
+            <img src="/alef_bet_wheel.png" class="app_ad" />
+        </IonCard>
+        <IonCard button="true" href="https://apps.apple.com/us/app/write-it-hebrew/id1400944424">
+            <img src="/write_it_hebrew.png" class="app_ad" />
+        </IonCard>
       </IonContent>
     </IonPage>
   );
